86e0e381eb33bf5079f96e3db68139e616c351c2
Nguyễn Nhật Tân <tanlxag116@gmail.com>
2025-04-04 09:58:09 +0700

    Colors

--- app/src/main/java/com/example/studentinformationmanagement/ui/shared/LoginScreen.kt
+++ app/src/main/java/com/example/studentinformationmanagement/ui/shared/LoginScreen.kt
@@ -1,11 +1,9 @@
 package com.example.studentinformationmanagement.ui.shared
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.studentinformationmanagement.ui.theme.MyPurple
 
 @Composable
 fun LoginScreen(
@@ -15,6 +13,8 @@ fun LoginScreen(
     // Gọi UiState của trang để sử dụng
     val loginUiState by loginViewModel.uiState.collectAsState()
 
-    // Giao diện của LoginScreen được viết tại đây và được xử lý bằng các hàm được gọi từ loginViewModel
+    /* Giao diện của LoginScreen được viết tại đây
+     * Và được xử lý bằng các hàm được gọi từ loginViewModel
+     **/
 
 }
