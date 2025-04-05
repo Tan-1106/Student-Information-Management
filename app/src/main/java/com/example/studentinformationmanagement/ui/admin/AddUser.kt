6a9fedfc81bd7db44a58a950e800a3ec43ec93d9
XungChan <nmdtruong18032004@gmail.com>
2025-04-05 15:34:44 +0700

    DetailUser

--- app/src/main/java/com/example/studentinformationmanagement/ui/admin/AddUser.kt
+++ app/src/main/java/com/example/studentinformationmanagement/ui/admin/AddUser.kt
@@ -1,2 +1,10 @@
 package com.example.studentinformationmanagement.ui.admin
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showSystemUi = true)
+fun AddUser(){
+
+}
