2176281ecfabe327954c34033090358bcdc53bdc
XungChan <nmdtruong18032004@gmail.com>
2025-04-05 14:52:40 +0700

    color.kt

--- app/src/main/java/com/example/studentinformationmanagement/ui/theme/Color.kt
+++ app/src/main/java/com/example/studentinformationmanagement/ui/theme/Color.kt
@@ -1,14 +1,20 @@
 package com.example.studentinformationmanagement.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.studentinformationmanagement.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
+val primary_content = Color(0xFFD45F5F)
+val secondary_content = Color(0xFFE9A098)
+val primary_container = Color(0xFFDDCACE)
+val secondary_dark = Color(0xFF7C7B6F)
+val primary_dark = Color(0xFF3A362E)
 
