ae7001602bb77fe71154508609e4a5081317613d
XungChan <nmdtruong18032004@gmail.com>
2025-04-19 13:41:41 +0700

    merce conflic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.2.2")
-
+    implementation("com.google.firebase:firebase-firestore-ktx")
     implementation(platform( libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation("com.google.firebase:firebase-auth-ktx")
