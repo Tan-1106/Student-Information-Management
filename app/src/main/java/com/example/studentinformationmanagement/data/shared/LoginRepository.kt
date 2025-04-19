3be35203d6307c282f879b36475c202e39720216
Emper <minhtruongb16@gmail.com>
2025-04-19 13:17:36 +0700

    Update UserManagement Screen

--- app/src/main/java/com/example/studentinformationmanagement/data/shared/LoginRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.studentinformationmanagement.data.shared
-
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.tasks.await
-
-class LoginRepository {
-    private val auth = Firebase
-}
