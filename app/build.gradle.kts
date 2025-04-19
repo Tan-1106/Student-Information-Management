6762e2947e14aeb1bc712a244bdae394709b9198
XungChan <nmdtruong18032004@gmail.com>
2025-04-19 13:43:00 +0700

    fix conflic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,6 @@ dependencies {
     implementation(platform( libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
     apply(plugin = "com.google.gms.google-services")
 
 }
