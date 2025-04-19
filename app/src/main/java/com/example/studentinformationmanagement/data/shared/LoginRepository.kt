6aa73a56a9b2aa6a357d7dc0b2c893d1af56d852
Nguyễn Nhật Tân <tanlxag116@gmail.com>
2025-04-19 12:31:28 +0700

    Create LoginRepository.kt

---
+++ app/src/main/java/com/example/studentinformationmanagement/data/shared/LoginRepository.kt
@@ -0,0 +1,8 @@
+package com.example.studentinformationmanagement.data.shared
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+class LoginRepository {
+    private val auth = Firebase
+}
