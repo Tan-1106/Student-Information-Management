afd97dc0803e9dce63543a937575c3ab52048e11
Emper <minhtruongb16@gmail.com>
2025-04-25 21:18:32 +0700

    Update

--- app/src/main/java/com/example/studentinformationmanagement/ui/shared/StudentCertificateList.kt
+++ app/src/main/java/com/example/studentinformationmanagement/ui/shared/StudentCertificateList.kt
@@ -1,12 +1,9 @@
 package com.example.studentinformationmanagement.ui.shared
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import me.saket.swipe.SwipeAction
 import me.saket.swipe.SwipeableActionsBox
 
